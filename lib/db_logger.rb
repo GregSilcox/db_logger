51552e0463aaffc5b49908d7f4e8ed73ec3e3e7e
Greg Silcox <greg.silcox@cumulsity.com>
2012-12-04 16:40:38 -0800

    Added documentation that rdoc will pick up.

--- lib/db_logger.rb
+++ lib/db_logger.rb
@@ -3,7 +3,12 @@ require 'db_logger/model_additions'
 require 'db_logger/railtie' if defined? Rails
 
 module DbLogger
-  # Code goes here...
+  # Currently this just returns your message with a prefix.
+  #
+  # DbLogger.debug "This is my message."
+  #
+  # This will return "Debug: This is my message."
+  #
   def self.debug message
     return "Debug: #{ message }"
   end
